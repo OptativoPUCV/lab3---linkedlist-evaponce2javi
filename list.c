e4d40fcac6b4199993ab6edcfd0c4f9ccc8ceec9
Your Name <you@example.com>
2024-03-28 19:12:48 +0000

    new try

--- list.c
+++ list.c
@@ -40,44 +40,44 @@ void *firstList(List *list) {
   if (list->head != NULL) {
     list->current = list->head;
     return list->current->data;
-
-    return NULL;
   }
-  void *nextList(List * list) {
-    if (list->current < list->tail - 1) {
-      list->current++;
-      return list->current;
-    }
-    return list->current->data;
+  return NULL;
+}
+void *nextList(List *list) {
+  if (list->current < list->tail - 1) {
+    list->current++;
+    return list->current;
   }
+  return list->current->data;
+}
 
-  void *lastList(List * list) { return NULL; }
+void *lastList(List *list) { return NULL; }
 
-  void *prevList(List * list) { return NULL; }
+void *prevList(List *list) { return NULL; }
 
-  void pushFront(List * list, void *data) {}
+void pushFront(List *list, void *data) {}
 
-  void pushBack(List * list, void *data) {
-    list->current = list->tail;
-    pushCurrent(list, data);
+void pushBack(List *list, void *data) {
+  list->current = list->tail;
+  pushCurrent(list, data);
+}
+
+void pushCurrent(List *list, void *data) {}
+
+void *popFront(List *list) {
+  list->current = list->head;
+  return popCurrent(list);
+}
+
+void *popBack(List *list) {
+  list->current = list->tail;
+  return popCurrent(list);
+}
+
+void *popCurrent(List *list) { return NULL; }
+
+void cleanList(List *list) {
+  while (list->head != NULL) {
+    popFront(list);
   }
-
-  void pushCurrent(List * list, void *data) {}
-
-  void *popFront(List * list) {
-    list->current = list->head;
-    return popCurrent(list);
-  }
-
-  void *popBack(List * list) {
-    list->current = list->tail;
-    return popCurrent(list);
-  }
-
-  void *popCurrent(List * list) { return NULL; }
-
-  void cleanList(List * list) {
-    while (list->head != NULL) {
-      popFront(list);
-    }
-  }
+}
